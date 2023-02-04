19c1eae7db129542da3555f3811e8ed13f026675
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2023-02-04 23:27:26 +0300

    Added zio-kafka example

--- build.sbt
+++ build.sbt
@@ -2,15 +2,10 @@ name := "kafka-avro"
 
 version := "1.0"
 
-scalaVersion := "2.13.10"
+scalaVersion := "3.2.1"
 
 libraryDependencies ++= Seq(
       "org.apache.avro" % "avro" % "1.11.1",
       "com.github.fd4s" %% "fs2-kafka" % "3.0.0-M8",
-      "org.apache.kafka" % "kafka_2.13" % "3.3.2",
-     // "io.confluent" % "kafka-avro-serializer" % "5.3.0",
-      "org.apache.kafka" % "kafka-examples" % "3.3.2",
-      "ch.qos.logback" %  "logback-classic" % "1.4.5",
-      "org.apache.kafka" % "kafka-streams" % "3.3.2",
-      "org.scalatest" %% "scalatest" % "3.3.0-SNAP3" % Test
+      "dev.zio" %% "zio-kafka" % "2.0.6"
 )
---
+++ src/main/scala/zio-kafka-demo.scala
@@ -0,0 +1,52 @@
+import zio._
+import zio.kafka.consumer._
+import zio.kafka.producer.{Producer, ProducerSettings}
+import zio.kafka.serde._
+import zio.stream.ZStream
+
+object MainApp extends ZIOAppDefault {
+  val producer: ZStream[Producer, Throwable, Nothing] =
+    ZStream
+      .repeatZIO(Random.nextIntBetween(0, Int.MaxValue))
+      .schedule(Schedule.fixed(2.seconds))
+      .mapZIO { random =>
+        Producer.produce[Any, Long, String](
+          topic = "random",
+          key = random % 4,
+          value = random.toString,
+          keySerializer = Serde.long,
+          valueSerializer = Serde.string
+        )
+      }
+      .drain
+
+  val consumer: ZStream[Consumer, Throwable, Nothing] =
+    Consumer
+      .subscribeAnd(Subscription.topics("random"))
+      .plainStream(Serde.long, Serde.string)
+      .tap(r => Console.printLine(r.value))
+      .map(_.offset)
+      .aggregateAsync(Consumer.offsetBatches)
+      .mapZIO(_.commit)
+      .drain
+
+  def producerLayer =
+    ZLayer.scoped(
+      Producer.make(
+        settings = ProducerSettings(List("localhost:9092"))
+      )
+    )
+
+  def consumerLayer =
+    ZLayer.scoped(
+      Consumer.make(
+        ConsumerSettings(List("localhost:9092")).withGroupId("group")
+      )
+    )
+
+  override def run =
+    producer
+      .merge(consumer)
+      .runDrain
+      .provide(producerLayer, consumerLayer)
+}
